91b07c49a6d49393eaaf34d591453be77a0813c6
minjin <tm003512@gmail.com>
2025-06-04 14:40:44 +0900

    Member 기능 구현 - UI

---
+++ src/main/java/org/example/semiproject/member/controller/api/MemberController.java
@@ -0,0 +1,25 @@
+package org.example.semiproject.member.controller.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/join")
+    public String join() {
+        return "views/member/join";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "views/member/login";
+    }
+
+    @GetMapping("/myinfo")
+    public String myinfo() {
+        return "views/member/myinfo";
+    }
+}
